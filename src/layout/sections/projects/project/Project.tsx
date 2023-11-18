86e579b1d7bd9b74abb883cc6c57d28d038fa63e
Arenelin <belov.1098@mail.ru>
2023-11-18 14:03:21 +0700

    fixed bugs and refactor code

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components"
 import { Button } from "../../../../components/Button"
 import { theme } from "../../../../styles/Theme"
 
-
 type ProjectPropsType = {
    src: string,
    title: string,
@@ -14,9 +13,9 @@ export function Project(props: ProjectPropsType) {
    return (
       <StyledProject>
          <WrapperContent>
-            <WrapperImage>
+            <ProjectImage>
                <Image src={props.src} alt="Project" />
-            </WrapperImage>
+            </ProjectImage>
             <ProjectInfo>
                <InfoTitle>{props.title}</InfoTitle>
                <InfoDescription>{props.descr}</InfoDescription>
@@ -31,14 +30,82 @@ export function Project(props: ProjectPropsType) {
    )
 }
 
-
 const StyledProject = styled.div`
 margin: 20px;
 `
+const ProjectImage = styled.div`
+width: 100%;
+height: 100%;
+overflow: hidden;
+position:absolute;
+transition: transform .7s ease;
+backface-visibility: hidden;
+-webkit-backface-visibility: hidden;
+transform: perspective(800px) rotateY(0);
+border: 3px solid transparent;
+`
+
+const Image = styled.img`
+object-fit: cover;
+width: 100%;
+height: 100%;
+`
+
+const ProjectInfo = styled.div`
+width: 100%;
+height: 100%;
+padding: 20px;
+border: 3px solid ${theme.colors.accentPrimary};
+transition: transform .7s ease;
+background: rgba(155, 144, 251, 0.08);
+text-align: center;
+
+overflow: hidden;
+position:absolute;
+backface-visibility: hidden;
+-webkit-backface-visibility: hidden;
+transform: perspective(800px) rotateY(180deg);
+
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: start;
+
+`
+
+const WrapperContent = styled.div`
+   width: 602px;  
+   height: 452px;
+   position: relative;
+
+   &:hover{
+   ${ProjectImage}{
+      transform: perspective(800px) rotateY(-180deg);
+   }
+   ${ProjectInfo}{
+      transform: perspective(800px) rotateY(0deg);
+   }
+}
+`
+
+const InfoTitle = styled.h3`
+padding-top: 40px;
+font-size: 35px;
+margin-bottom: 25px;
+font-weight: 600;
+letter-spacing: 1.5px;
+text-transform: capitalize;
+`
+
 const InfoDescription = styled.p`
 font-size: 18px;
 margin-bottom: 25px;
-text-align: center;
+`
+
+const InfoStack = styled.p`
+font-size: 20px;
+color: #a39d9d;
+margin-bottom: 35px;
 `
 
 const WrapperButtons = styled.div`
@@ -51,7 +118,7 @@ ${Button}{
    position: relative;
    border-radius: 0px;
    overflow: hidden;
-   padding: 10px 35px;
+   padding: 8px 40px;
    font-size: 20px;
 
    &:before {
@@ -77,71 +144,4 @@ ${Button}{
       }
    }
    }
-`
-
-const InfoTitle = styled.h3`
-padding-top: 40px;
-font-size: 35px;
-margin-bottom: 25px;
-font-weight: 600;
-letter-spacing: 1.5px;
-text-transform: capitalize;
-text-align: center;
-`
-const InfoStack = styled.p`
-font-size: 20px;
-color: #a39d9d;
-margin-bottom: 35px;
-text-align: center;
-`
-
-const WrapperImage = styled.div`
-width: 100%;
-height: 100%;
-overflow: hidden;
-position:absolute;
-transition: transform .7s ease;
-backface-visibility: hidden;
--webkit-backface-visibility: hidden;
-transform: perspective(800px) rotateY(0);
-border: 3px solid transparent;
-`
-
-const Image = styled.img`
-object-fit: cover;
-width: 100%;
-height: 100%;
-`
-
-const ProjectInfo = styled.div`
-width: 100%;
-height: 100%;
-padding: 20px;
-overflow: hidden;
-position:absolute;
-background: rgba(155, 144, 251, 0.08);
-backface-visibility: hidden;
--webkit-backface-visibility: hidden;
-transition: transform .7s ease;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: start;
-transform: perspective(800px) rotateY(180deg);
-border: 3px solid ${theme.colors.accentPrimary};
-`
-
-const WrapperContent = styled.div`
-   width: 602px;
-   height: 452px;
-   position: relative;
-
-   &:hover{
-   ${WrapperImage}{
-      transform: perspective(800px) rotateY(-180deg);
-   }
-   ${ProjectInfo}{
-      transform: perspective(800px) rotateY(0deg);
-   }
-}
 `
